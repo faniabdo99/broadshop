79b13a1a616d76cfbb94518cdad824c0acad0697
faniabdo99 <faniabdo99@gmail.com>
2021-05-29 17:50:25 +0200

    Latest

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,3 +1,6 @@
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NGHPW7N" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <!--header start-->
   <header class="site-header header-5">
     <div class="header-top bg-blue pt-1">
