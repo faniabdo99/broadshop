079a8aaed35f80514f739f2b9c1a95276980ede8
faniabdo99 <faniabdo99@gmail.com>
2021-06-02 02:41:24 +0200

    fd

--- resources/views/layout/scripts.blade.php
+++ resources/views/layout/scripts.blade.php
@@ -12,3 +12,29 @@
     <script src="{{url('public/')}}/js/wow.js"></script>
     <script src="{{url('public/')}}/js/app.js"></script>
 <!-- inject js end -->
+<!-- Messenger Chat plugin Code -->
+<div id="fb-root"></div>
+
+<!-- Your Chat plugin code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "106290844978466");
+  chatbox.setAttribute("attribution", "page_inbox");
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v10.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/nl_NL/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
