5af192ffac2556c7ea6adcfb73ef1c535bd5b348
faniabdo99 <faniabdo99@gmail.com>
2021-06-12 15:17:23 +0200

    Latest

--- resources/views/mail/orders/order-receipt-mail.blade.php
+++ resources/views/mail/orders/order-receipt-mail.blade.php
@@ -3,7 +3,7 @@
 @lang('mails/orders.order_receipt_first_paragraph') <br>
 <p><b>@lang('mails/orders.order_receipt_serial_number'): </b> {{$EmailData->serial_number}}</p><br>
 <p><b>@lang('mails/orders.order_receipt_order_total'): </b> {{$EmailData->total.getCurrency()['symbole']}}</p><br>
-<p><b>@lang('mails/orders.order_receipt_payment_method'): </b> {{$EmailData->PaymentMethodData['name']}}</p><br>
+<p><b>@lang('mails/orders.order_receipt_payment_method'): </b> {{$EmailData->PaymentMethodText}}</p><br>
 <p>@lang('mails/orders.order_receipt_last_paragraph')</p>
 @component('mail::button', ['url' => 'https://ukfashionshop.be'])
 @lang('mails/mails.sender')
