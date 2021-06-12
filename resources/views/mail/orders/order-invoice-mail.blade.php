235f598bff4d28ad686642e829cd54797e3788a2
faniabdo99 <faniabdo99@gmail.com>
2021-06-12 15:25:05 +0200

    Latest

--- resources/views/mail/orders/order-invoice-mail.blade.php
+++ resources/views/mail/orders/order-invoice-mail.blade.php
@@ -4,6 +4,13 @@
 <br>
 <b>@lang('mails/orders.order_invoice_order_id'):</b> {{$EmailData->serial_number}}<br>
 <b>@lang('mails/orders.order_invoice_total'):</b> {{$EmailData->total}}€<br>
+<h3>Your Order:</h3>
+| Product | Quanitity | Price | Total |
+|----------|:-------------:|------:||
+@forelse($EmailData->Items() as $Item)
+@empty
+| {{$Item->Product->title}} |  {{$Item->qty}} | {{$Item->Product->price}} | {{($Item->Product->price * $Item->qty)}} |
+@endforelse
 @lang('mails/mails.thanks'),<br>
 {{ config('app.name') }}
 @endcomponent
--- resources/views/mail/orders/order-receipt-mail.blade.php
+++ resources/views/mail/orders/order-receipt-mail.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
-# @lang('mails/orders.order_receipt')
-@lang('mails/orders.order_receipt_first_paragraph') <br>
-<p><b>@lang('mails/orders.order_receipt_serial_number'): </b> {{$EmailData->serial_number}}</p><br>
-<p><b>@lang('mails/orders.order_receipt_order_total'): </b> {{$EmailData->total.getCurrency()['symbole']}}</p><br>
-<p><b>@lang('mails/orders.order_receipt_payment_method'): </b> {{$EmailData->PaymentMethodText}}</p><br>
-<p>@lang('mails/orders.order_receipt_last_paragraph')</p>
-@component('mail::button', ['url' => 'https://ukfashionshop.be'])
-@lang('mails/mails.sender')
+# Your Order Receipt From Broadshop
+<p>Thank you for your order at UK Fashion Shop, Please find the order information below</p><br>
+<p><b>Order Serial Number:</b> {{$EmailData->serial_number}}</p>
+<p><b>Order Total: </b> {{$EmailData->total.getCurrency()['symbole']}}</p>
+<p><b>Payment Method: </b> {{$EmailData->PaymentMethodText}}</p><br>
+<p>if you already have an account, you can view your orders from your account page at Broadshop.</p>
+@component('mail::button', ['url' => 'https://broadshop.be'])
+    Broadshop.be
 @endcomponent
 
-@lang('mails/mails.thanks'),<br>
-{{ config('app.name') }}
+Thank You,<br>
+Broadshop
 @endcomponent
