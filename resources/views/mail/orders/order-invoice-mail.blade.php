0f03028fab652fbf4ded95037d95ca45025487e9
faniabdo99 <faniabdo99@gmail.com>
2021-06-12 15:30:37 +0200

    Latest

--- resources/views/mail/orders/order-invoice-mail.blade.php
+++ resources/views/mail/orders/order-invoice-mail.blade.php
@@ -4,26 +4,7 @@
 <br>
 <b>@lang('mails/orders.order_invoice_order_id'):</b> {{$EmailData->serial_number}}<br>
 <b>@lang('mails/orders.order_invoice_total'):</b> {{$EmailData->total}}€<br>
-<h3>Your Order:</h3>
-<table>
-    <thead>
-        <th>Product</th>
-        <th>Quanitity</th>
-        <th>Price</th>
-        <th>Total</th>
-    </thead>
-    <tbody>
-        @forelse($EmailData->Items() as $Item)
-        @empty
-        <tr>
-            <td>{{$Item->Product->title}}</td>
-            <td>{{$Item->qty}}</td>
-            <td>{{$Item->Product->price}}</td>
-            <td>{{($Item->Product->price * $Item->qty)}}</td>
-        </tr>
-        @endforelse
-    </tbody>
-</table>
+
 Thanks<br>
 Broadshop
 @endcomponent
--- resources/views/mail/orders/order-receipt-mail.blade.php
+++ resources/views/mail/orders/order-receipt-mail.blade.php
@@ -4,6 +4,26 @@
 <p><b>Order Serial Number:</b> {{$EmailData->serial_number}}</p>
 <p><b>Order Total: </b> {{$EmailData->total.getCurrency()['symbole']}}</p>
 <p><b>Payment Method: </b> {{$EmailData->PaymentMethodText}}</p><br>
+<h3>Your Order:</h3>
+<table>
+    <thead>
+        <th>Product</th>
+        <th>Quanitity</th>
+        <th>Price</th>
+        <th>Total</th>
+    </thead>
+    <tbody>
+        @forelse($EmailData->Items() as $Item)
+        @empty
+        <tr>
+            <td>{{$Item->Product->title}}</td>
+            <td>{{$Item->qty}}</td>
+            <td>{{$Item->Product->price}}</td>
+            <td>{{($Item->Product->price * $Item->qty)}}</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
 <p>if you already have an account, you can view your orders from your account page at Broadshop.</p>
 @component('mail::button', ['url' => 'https://broadshop.be'])
     Broadshop.be
